324c0023386c9716a91047f3e1807943c7beba0e
wsj1983 <wsj1983@gmail.com>
2019-04-03 22:11:37 -0300

    feat: add input robot and state robot

--- robots/input.js
+++ robots/input.js
@@ -1,18 +1,21 @@
 const readline = require('readline-sync')
+const state = require('./state.js')
 
 function robot() {
-  const content = {}
+  const content = {
+    maximumSentences: 7
+  }
 
   content.searchTerm = askAndReturnSearchTerm()
   content.prefix = askAndReturnPrefix()
-
+  state.save(content)
 
   function askAndReturnSearchTerm() {
-    return readline.question('Type a Wikipedia search term: ')
+    return readline.question('Type a wikipedia search term:')
   }
 
   function askAndReturnPrefix() {
-    const prefixes = ['Who is', 'What is', 'The history of']
+    const prefixes = ['who is', 'what is', 'the history of']
     const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
     const selectedPrefixText = prefixes[selectedPrefixIndex]
 
